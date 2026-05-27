c85290f247d35c8479a45a35d22220c249d60519
Bien Sarmiento <60124509+BnS2@users.noreply.github.com>
2026-05-28 01:04:11 +0800

    chore: add Varlock for typed environment variable management

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
+const { withVarlockMetroConfig } = require("@varlock/expo-integration/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativewind(config);
+module.exports = withVarlockMetroConfig(withNativewind(config));
